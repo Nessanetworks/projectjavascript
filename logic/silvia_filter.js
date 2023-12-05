910505a096d8a7d87868c18ddf0438231d5306f2
Silviaip <143509374+Silviaip@users.noreply.github.com>
2023-12-05 23:45:40 +0100

    hej

--- logic/silvia_filter.js
+++ logic/silvia_filter.js
@@ -1,5 +1,9 @@
-function get_random_number(tal1, tal5000)("addidas")("nike")("uggs") {
+function get_random_number(tal1, tal5000)("addidas")("nike")("uggs"); {
     var summa = (tal1,tal5000)
+   
     return summa;
   }
   var resultat = random (tal1,tal5000);
+
+   var array = ("addidas")("nike")("uggs")
+return array
