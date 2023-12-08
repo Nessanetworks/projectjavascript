771e84258200ea2a7d323cd056cb43721471865b
oucheoof <stephendoan@hotmail.com>
2023-12-08 16:58:29 +0100

    Skapat knapparna
    Kvar att göra: koppla maxpris funktioner till knappar & göra maxpris funktion variablet.

--- logic/max_price_filter.js
+++ logic/max_price_filter.js
@@ -1,4 +1,5 @@
 "use strict";
+
 function max_price_500(shoe_array, max_price) {
     let new_array = [];
     for (let shoe of shoe_array) {
@@ -34,12 +35,26 @@ function  max_price_700(shoe_array, max_price) {
 };
 console.log(max_price_700(SHOES, 700,));
 
-function createInput (){
-    let newInput = document.createElement("input");
-    newInput.setAttribute("checkbox", "")
-    let parent = document.querySelector("body")
+function createCheckbox (){
+    //Variables
+    let amountOfCheckBoxes = 0;
+    let maxAmountOfCheckBoxes = 3;
 
-    parent.appendChild(newInput)
+    for (;amountOfCheckBoxes < maxAmountOfCheckBoxes; amountOfCheckBoxes++){ //vet ej varför semi colon fungerar så här men ok
+
+    //DOM
+    let parent = document.querySelector("body");
+    let newDiv = document.createElement("div");
+    let newParagraph  = document.createElement("p"); newParagraph.textContent = 500;
+    let newInput = document.createElement("input"); newInput.setAttribute("type", "checkbox");
+    
+    //apend
+    parent.appendChild(newDiv);
+    newDiv.appendChild(newParagraph);
+    newDiv.appendChild(newParagraph);
+    newDiv.appendChild(newInput);
+    };
+
 }
 
-createInput();
+createCheckbox();
