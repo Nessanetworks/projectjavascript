9c99ecdf2e6eefb95936c1380044560c3c2fcef2
Johanna <johanna.hallinger99@gmail.com>
2023-12-11 14:10:03 +0100

    Update main.js

--- components/main/main.js
+++ components/main/main.js
@@ -4,22 +4,18 @@ function render_bottom_container (shoes) {
 
     const main = document.querySelector("main");
     const bottom_container = document.querySelector("#bottom_container");
+        
+    for (let i = 0; i < shoes.length; i++) {
+    bottom_container.innerHTML = `
     
-    const bottom_div = document.createElement("div");
-    
-    bottom_div.innerHtml = `
-    
-    <div>
-        <img src="media/sko_biler${shoes.file_name}">
-        <h1>${shoes.name}<h1>
-        <p>${shoes.price}</p>
-    </div>`;
-    
-    
-    for (i = 0; i < shoes.length; i++) {
-       
+        <div>
+            <img src="media/sko_bilder${shoes.file_name}">
+            <h1>${shoes.name}</h1>
+            <p>Country</p>
+            <p>${shoes.price}</p>
+        </div>`;
+        
     }
 
     main.appendChild(bottom_container);
-    bottom_container.appendChild(bottom_div);
 }
