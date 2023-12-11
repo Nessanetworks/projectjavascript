d59a9e6046d15bceefad174e2df73d45537e951f
oucheoof <stephendoan@hotmail.com>
2023-12-11 18:49:30 +0100

    Update max_price_filter.js

--- logic/max_price_filter.js
+++ logic/max_price_filter.js
@@ -8,7 +8,7 @@ Koppla filter till knapparna
 
 "use strict";
 
-function max_price_500(shoe_array, max_price) {
+function filter_price(shoe_array, max_price) { 
     let new_array = [];
     for (let shoe of shoe_array) {
         if (shoe.price <= max_price) {
@@ -17,9 +17,9 @@ function max_price_500(shoe_array, max_price) {
     }
     return new_array;
 };
-console.log(max_price_500(SHOES, 500,));
+console.log(filter_price(SHOES, 500,));
 
-function  max_price_600(shoe_array, max_price) {
+/* function  max_price_600(shoe_array, max_price) {
     let new_array = [];
     for (let shoe of shoe_array) {
         if (shoe.price <= max_price) {
@@ -41,10 +41,10 @@ function  max_price_700(shoe_array, max_price) {
     return new_array;
 
 };
-console.log(max_price_700(SHOES, 700,));
+console.log(max_price_700(SHOES, 700,)); */
 
-function createCheckbox(numbers) {
-    // Variables
+function createCheckbox(numbers, shoe_array) {
+
     for (let i = 0; i < numbers.length; i++) {
         
         // DOM
@@ -57,6 +57,12 @@ function createCheckbox(numbers) {
         newLabel.setAttribute("for", "checkbox_" + i); // Set the 'for' attribute of the label to match the checkbox id
         newLabel.textContent = numbers[i] + " kr"; // Set the text content to the number and " kr"
 
+        (function (max_price) {
+            newInput.addEventListener("change", function () {
+                handleCheckboxChange(max_price, shoe_array, newInput);
+            });
+        })(numbers[i]);
+
         // Append
         parent.appendChild(newDiv);
         newDiv.appendChild(newInput);
@@ -64,9 +70,20 @@ function createCheckbox(numbers) {
     }
 }
 
+function handleCheckboxChange(max_price, shoe_array, checkbox) {
+    // Check if the checkbox is checked
+    if (checkbox.checked) {
+        // If checked, filter the array based on the max_price
+        let filteredShoes = filter_price(shoe_array, max_price);
+        console.log(filteredShoes);
+    } else {
+        // If not checked, show all shoe prices
+        console.log(shoe_array);
+    }
+}
 // Example usage with variable numbers
 let customNumbers = [500, 600, 700];
-createCheckbox(customNumbers);
+createCheckbox(customNumbers, SHOES);
 
 
 
