a06aa4528a6ff303e638e169452ac958606c7ff9
ThXzZzZ <nilssontaxel@gmail.com>
2023-12-11 14:51:29 +0100

    fixat mycket

--- components/main/main.js
+++ components/main/main.js
@@ -5,17 +5,19 @@ function render_bottom_container (shoes) {
     const main = document.querySelector("main");
     const bottom_container = document.querySelector("#bottom_container");
         
-    for (let i = 0; i < shoes.length; i++) {
-    bottom_container.innerHTML = `
-    
-        <div>
-            <img src="media/sko_bilder${shoes.file_name}">
-            <h1>${shoes.name}</h1>
+    for (const shoe of shoes) {
+        const bottom_div = document.createElement("div");
+        bottom_div.classList.add("bottom_div");
+    bottom_div.innerHTML = `
+            
+            <img src="media/sko_bilder/${shoe.file_name}">
+            <h1>${shoe.name}</h1>
             <p>Country</p>
-            <p>${shoes.price}</p>
-        </div>`;
-        
+            <p>${shoe.price}</p>
+        `;
+        bottom_container.appendChild(bottom_div);
     }
 
     main.appendChild(bottom_container);
 }
+
