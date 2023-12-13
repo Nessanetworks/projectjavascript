927397a985df2030abbb10a32386cbd71dbea1b9
Johanna <johanna.hallinger99@gmail.com>
2023-12-13 10:33:25 +0100

    added kind and country

--- components/main/main.js
+++ components/main/main.js
@@ -6,14 +6,20 @@ function render_bottom_container (shoes) {
     const bottom_container = document.querySelector("#bottom_container");
         
     for (const shoe of shoes) {
+        
         const bottom_div = document.createElement("div");
         bottom_div.classList.add("bottom_div");
+
+        const country = array_find(COUNTRIES, function(country){return country.id === shoe.country_id;});
+        const kind = array_find(KINDS, function (kind){return kind.id === shoe.kind_id;});
+
     bottom_div.innerHTML = `
             
             <img src="media/sko_bilder/${shoe.file_name}">
             <h1>${shoe.name}</h1>
-            <p>Country</p>
-            <p>${shoe.price}</p>
+            <p>${kind.name}</p>
+            <p>${country.name}</p>
+            <p>${shoe.price}kr</p>
         `;
         bottom_container.appendChild(bottom_div);
     }
