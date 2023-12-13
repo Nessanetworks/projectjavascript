6a4a5302fd3d503485f628f0d8802f1ff12d925a
Nessanetworks <Nessanetworks@users.noreply.github.com>
2023-12-13 10:56:48 +0100

    Filter för TYP, färdigt

--- logic/filter_typ.js
+++ logic/filter_typ.js
@@ -1,6 +1,40 @@
 "use strict"
 
-function filterShoesByType(shoeArray, type) {
+function filter_by_type() {
+
+  let checked_kind = document.querySelectorAll(".type_option_container .checked .text");
+
+  let checked_textContent_array = array_map(checked_kind, function (element) {
+    return element.textContent;
+  });
+
+  let kindsArray = array_map(checked_textContent_array, function (name) {
+    return array_find(KINDS, function (kind) {
+      return name === kind.name;
+    });
+  });
+
+  let kindId = array_map(kindsArray, function (kind) {
+    return kind.id;
+  });
+
+  let program = array_filter(SHOES, function (shoe) {
+    return kindId.includes(shoe.kind_id);
+  });
+
+  console.log(program);
+
+  return program;
+}
+
+
+
+
+
+
+
+
+/*function filterShoesByType(shoeArray, type) {
   const result = [];
   for (let i = 0; i < shoeArray.length; i++) {
     if (shoeArray[i].kind_id === type) {
@@ -12,7 +46,7 @@ function filterShoesByType(shoeArray, type) {
 
 const slippers = filterShoesByType(SHOES, 1);
 const boots = filterShoesByType(SHOES, 2);
-const sneakers = filterShoesByType(SHOES, 3);
+const sneakers = filterShoesByType(SHOES, 3);*/
 
 
 
