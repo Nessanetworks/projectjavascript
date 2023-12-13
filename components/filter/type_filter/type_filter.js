f8d9769dca14290371c209d29058b7a97d673f9f
albagirlinger <143420647+albagirlinger@users.noreply.github.com>
2023-12-13 08:18:44 +0100

    Update type_filter.js

--- components/filter/type_filter/type_filter.js
+++ components/filter/type_filter/type_filter.js
@@ -1,12 +1,12 @@
-"use strict"; 
-/*
+"use strict";
+
 function render_type_filters(parent) {
 
     const container = document.createElement("div");
     container.id = "type_filters";
     parent.append(container);
 
-    for(let kind of KINDS) {
+    for (let kind of KINDS) {
         const type_container = document.createElement("div");
         container.append(type_container);
         type_container.classList.add("type_container");
@@ -17,10 +17,10 @@ function render_type_filters(parent) {
         `;
     }
 }
-*/
 
-/*
-function render_filter_box (parent, text) {
+
+
+function render_filter_box(parent, text) {
 
     const container = document.createElement("div");
     parent.appendChild(container);
@@ -29,18 +29,17 @@ function render_filter_box (parent, text) {
     const type_container = document.createElement("div");
     container.appendChild(type_container);
 
-    for(let kind of KINDS) {
+    for (let kind of KINDS) {
         const check_container = document.createElement("div");
         container.append(check_container);
         check_container.classList.add("type_container");
 
         check_container.textContent = `${kind.name}`;
-        /*
+
         type_container.innerHTML = `
         <div>TYP</div>
         <p>${kind.name}</p>
         <div class="type_container"></div>
-        `; 
+        `;
     }
 }
-*/
