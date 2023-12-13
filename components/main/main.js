d322774f453671c11640e6c78824720b21d212ff
Johanna <johanna.hallinger99@gmail.com>
2023-12-14 00:49:22 +0100

    .

--- components/main/main.js
+++ components/main/main.js
@@ -7,13 +7,15 @@ function render_bottom_container (shoes) {
         
     for (const shoe of shoes) {
         
+        // Skapar vanlig div: 
+
         const bottom_div = document.createElement("div");
         bottom_div.classList.add("bottom_div");
 
         const country = array_find(COUNTRIES, function(country){return country.id === shoe.country_id;});
         const kind = array_find(KINDS, function (kind){return kind.id === shoe.kind_id;});
 
-    bottom_div.innerHTML = `
+        bottom_div.innerHTML = `
             
             <img src="media/sko_bilder/${shoe.file_name}">
             <h1>${shoe.name}</h1>
@@ -23,21 +25,24 @@ function render_bottom_container (shoes) {
         `;
         bottom_container.appendChild(bottom_div);
 
+        // Skapar popup div:
+        
         bottom_div.addEventListener("click", function() {
+            
             const bottom_div_popup = document.createElement("div");
-            bottom_div_popup.classList.toggle("bottom_div_popup");
+            bottom_div_popup.classList.add("bottom_div_popup");
+            bottom_container.appendChild(bottom_div_popup);
 
             bottom_div_popup.innerHTML = `
             
-            <img src="media/sko_bilder/${shoe.file_name}">
-            <h1>${shoe.name}</h1>
-            <p>${kind.name}</p>
-            <p>${country.name}</p>
-            <p>${shoe.price}kr</p>
-            <img src="media/X.png" id="x">
-            
-            `
-            bottom_container.appendChild(bottom_div_popup);
+                <img src="media/sko_bilder/${shoe.file_name}">
+                <h1>${shoe.name}</h1>
+                <p>${kind.name}</p>
+                <p>${country.name}</p>
+                <p>${shoe.price}kr</p>
+                <img src="media/X.png" id="close_x">
+            `;
+
         });
     }
 
