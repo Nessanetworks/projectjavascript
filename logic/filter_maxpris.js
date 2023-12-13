e9194edb5aa7b486dba16916058831495b1c1783
oucheoof <143434428+oucheoof@users.noreply.github.com>
2023-12-13 15:12:26 +0100

    Update filter_maxpris.js

--- logic/filter_maxpris.js
+++ logic/filter_maxpris.js
@@ -1,32 +1,19 @@
 "use strict";
 
-function filter_by_price  (){
+function filter_by_price(){
     let checked_country = document.querySelectorAll(".price_option_container .checked .text");
-function filter_by_country(){
-    let checked_country = document.querySelectorAll(".country_option_container .checked .text");
-
     let checked_textContent_array = array_map(checked_country, function(element){
         return element.textContent;
     });
-
-    let countriesArray = array_map(checked_textContent_array, function(name){
-        return array_find(COUNTRIES, function(country){
-            return name === country.name;
-        }); 
-    });
-
-    let countryId = array_map(countriesArray, function(country){
-        return country.id;
-    });
-
-    let program = array_filter(SHOES, function(shoe){
-        return countryId.includes(shoe.country_id);
-    });
-
-    console.log(program);
-
-    return program;  
-}
+    let result = [];
+    for(let number of checked_textContent_array){
+        for(let shoe of SHOES){
+            if(shoe.price <= number){
+                result.push(shoe);
+            }
+        }
+    }
+    console.log(result);
 
 function filter_review(shoe){
     let array_shoe_review = array_filter(REVIEWS, function(review){
