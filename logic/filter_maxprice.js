d81f13df0ae594c1e0c5dfd23723d1a552d25838
Nessanetworks <Nessanetworks@users.noreply.github.com>
2023-12-13 15:54:00 +0100

    Update filter_maxprice.js

--- logic/filter_maxprice.js
+++ logic/filter_maxprice.js
@@ -1,32 +1,16 @@
+
 function filter_by_maxprice() {
     let checked_country = document.querySelectorAll(".price_option_container .checked .text");
-
     let checked_textContent_array = array_map(checked_country, function (element) {
         return element.textContent;
     });
-
-    let result = SHOES.filter(function (shoe) {
-        return checked_textContent_array.some(function (maxPrice) {
-            return shoe.price <= maxPrice;
-        });
-    });
-
-    console.log(result);
-};
-
-
-/*function filter_by_maxprice(){
-    let checked_country = document.querySelectorAll(".price_option_container .checked .text");
-    let checked_textContent_array = array_map(checked_country, function(element){
-        return element.textContent;
-    });
     let result = [];
-    for(let number of checked_textContent_array){
-        for(let shoe of SHOES){
-            if(shoe.price <= number){
+    for (let number of checked_textContent_array) {
+        for (let shoe of SHOES) {
+            if (shoe.price <= number) {
                 result.push(shoe);
             }
         }
     }
     console.log(result);
-};*/
+};
