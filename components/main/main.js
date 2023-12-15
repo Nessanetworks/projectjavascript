8b7130febedec24a4a17bf35d868197675d697b5
Johanna <johanna.hallinger99@gmail.com>
2023-12-15 16:25:55 +0100

    close popup

--- components/main/main.js
+++ components/main/main.js
@@ -5,11 +5,15 @@ function render_bottom_container (shoes) {
 
     const main = document.querySelector("main");
     const bottom_container = document.querySelector("#bottom_container");
-        
+    
+    const bottom_container_popup = document.createElement("div");
+
     for (const shoe of shoes) {
         
         const country = array_find(COUNTRIES, function(country){return country.id === shoe.country_id;});
         const kind = array_find(KINDS, function (kind){return kind.id === shoe.kind_id;});
+        const review = array_find(REVIEWS, function(review){return review.shoe_id === shoe.id});
+        const score = array_find(REVIEWS, function(review){return review.score === shoe.id});
 
         // Skapar vanlig div: 
 
@@ -22,16 +26,14 @@ function render_bottom_container (shoes) {
             <h1>${shoe.name}</h1>
             <p>${kind.name}</p>
             <p>${country.name}</p>
-            <p>${shoe.price}kr</p>
+            <p id="shoe_price">${shoe.price} kr</p>
         `;
-        bottom_container.appendChild(bottom_div);
         
-        // Skapar popup div: 
-
+        // Öppnar popup container
+        
         bottom_div.addEventListener("click", function() {
-            const bottom_container_popup = document.createElement("div");
             bottom_container_popup.classList.add("bottom_container_popup");
-
+            
             bottom_container_popup.innerHTML = ` 
             <div>
                 <img src="media/sko_bilder/${shoe.file_name}">
@@ -40,17 +42,28 @@ function render_bottom_container (shoes) {
                 <h1>${shoe.name}</h1>
                 <p>${kind.name}</p>
                 <p>${country.name}</p>
-                <p>${shoe.price}kr</p> 
+                <p id="shoe_price">${shoe.price} kr</p>
+                <div> 
+                    <h2>Reviews</h2>
+                    <p>"${review.rev}"</p>
+                    <h3>Rating</h3>
+                    <p>${review.score}/5</p>
+                </div> 
             </div>
-            <button>X</button>
+            <button class="popup_close_button">X</button>
         `;
-            bottom_container.appendChild(bottom_container_popup);
+        
+        // Stänger popup container 
+
+        bottom_container_popup.addEventListener("click", function () {
+            bottom_container.removeChild(bottom_container_popup);
         });
 
-    };
+        bottom_container.appendChild(bottom_container_popup);
+    });
 
-    main.appendChild(bottom_container);
+    bottom_container.appendChild(bottom_div);
+}
 
-};
-
-// <button onclick="closePopup()">X</button>
+main.appendChild(bottom_container);
+}
