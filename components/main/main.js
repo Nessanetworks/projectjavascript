bdae52cead362d7ab03ef1a237345cab34f5a315
Johanna <johanna.hallinger99@gmail.com>
2023-12-18 12:24:29 +0100

    .

--- components/main/main.js
+++ components/main/main.js
@@ -1,40 +1,32 @@
+"use strict";
 
-"use strict"
+function render_bottom_container(shoes) {
+  const main = document.querySelector("main");
+  const bottom_container = document.querySelector("#bottom_container");
 
-function render_bottom_container (shoes) {
-
-    const main = document.querySelector("main");
-    const bottom_container = document.querySelector("#bottom_container");
+  for (const shoe of shoes) {
     
-    const bottom_container_popup = document.createElement("div");
+    const country = array_find(COUNTRIES, function (country) {return country.id === shoe.country_id;});
+    const kind = array_find(KINDS, function (kind) {return kind.id === shoe.kind_id;});
 
-    for (const shoe of shoes) {
-        
-        const country = array_find(COUNTRIES, function(country){return country.id === shoe.country_id;});
-        const kind = array_find(KINDS, function (kind){return kind.id === shoe.kind_id;});
-        const review = array_find(REVIEWS, function(review){return review.shoe_id === shoe.id});
-        const score = array_find(REVIEWS, function(review){return review.score === shoe.id});
+    // Skapar vanlig div:
+    const bottom_div = document.createElement("div");
+    bottom_div.classList.add("bottom_div");
 
-        // Skapar vanlig div: 
+    bottom_div.innerHTML = `
+        <img src="media/sko_bilder/${shoe.file_name}">
+        <h1>${shoe.name}</h1>
+        <p>${kind.name}</p>
+        <p>${country.name}</p>
+        <p id="shoe_price">${shoe.price} kr</p>
+    `;
 
-        const bottom_div = document.createElement("div");
-        bottom_div.classList.add("bottom_div");
+    // Öppnar popup container
+    bottom_div.addEventListener("click", function () {
+      const bottom_container_popup = document.createElement("div");
+      bottom_container_popup.classList.add("bottom_container_popup");
 
-        bottom_div.innerHTML = `
-            
-            <img src="media/sko_bilder/${shoe.file_name}">
-            <h1>${shoe.name}</h1>
-            <p>${kind.name}</p>
-            <p>${country.name}</p>
-            <p id="shoe_price">${shoe.price} kr</p>
-        `;
-        
-        // Öppnar popup container
-        
-        bottom_div.addEventListener("click", function() {
-            bottom_container_popup.classList.add("bottom_container_popup");
-            
-            bottom_container_popup.innerHTML = ` 
+      bottom_container_popup.innerHTML = `
             <div>
                 <img src="media/sko_bilder/${shoe.file_name}">
             </div>
@@ -44,33 +36,30 @@ function render_bottom_container (shoes) {
                 <p>${country.name}</p>
                 <p id="shoe_price">${shoe.price} kr</p>
                 <div id="reviews"> 
-                    <h2>Reviews</h2>
-                    <p>"${review.rev}"</p>
+                    <h1>Reviews</h1>
+                    <p>"Reviews"</p>
                     <h3>Rating</h3>
-                    <p>${review.score}/5</p>
+                    <p>"Reviews"</p>
                 </div> 
             </div>
         `;
-        
-        // Skapar button  
-        
-        const popup_close_button = document.createElement("button");
-        popup_close_button.setAttribute("id", "popup_close_button");
-        popup_close_button.textContent = "X";
-        bottom_container_popup.appendChild(popup_close_button);
 
-        // Stänger popup 
+      // Skapar button
+      const popup_close_button = document.createElement("button");
+      popup_close_button.id = "popup_close_button";
+      popup_close_button.textContent = "X";
+      bottom_container_popup.appendChild(popup_close_button);
 
-        popup_close_button.addEventListener("click", function () {
-            bottom_container.removeChild(bottom_container_popup);
-        });
+      // Stänger popup
+      popup_close_button.addEventListener("click", function () {
+        bottom_container_popup.classList.add("hide");
+      });
 
-        bottom_container.appendChild(bottom_container_popup);
+      bottom_container.appendChild(bottom_container_popup);
     });
 
     bottom_container.appendChild(bottom_div);
-}
+  }
 
-main.appendChild(bottom_container);
-
+  main.appendChild(bottom_container);
 }
